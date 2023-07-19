fefa80f20eb2b95a6dba4fd121b27b968578747b
Lukas Isermann <61188540+liserman@users.noreply.github.com>
2023-07-19 14:01:15 +0200

    Change function to be split into individual functions and write tests

--- R/handcode.R
+++ R/handcode.R
@@ -147,32 +147,6 @@ handcoder_app <- function(a) {
     ),
     server = function(input, output, session) {
 
-      # Function for output generation
-      gen_output <- function(data, values){
-
-        # Generate final data.frame to be displayed as output
-        final <- data.frame(id = data$id, texts = data$texts, kat1 = values$code1, kat2 = values$code2, kat3 = values$code3)
-
-        # Reduce to size of original dataframe
-        final <- final[,1:(ncol(data)-2)]
-
-        # Take names from original dataframe
-        names(final) <- names(data)[-c(2,3)]
-
-        # Make sure all NA is saved as ""
-        final[is.na(final)] <- ""
-
-        # Reorder output
-        final <- final[order(final$id),]
-
-        # Delete id
-        final <- final[,-1]
-        rownames(final) <- NULL
-
-        # Return
-        return(final)
-      }
-
       # Initialize reactiveValues
       values <- shiny::reactiveValues(
         # Set counter to start
@@ -272,119 +246,62 @@ handcoder_app <- function(a) {
 }
 
 
+#' gen_output: Combine data and values of shiny app to output
+gen_output <- function(data, values){
 
-#' handcode: Classifying text into pre-defined categories.
-#'
-#' `handcode` opens a Shiny app which allows for handcoding strings of text into pre-defined categories. You can code between one and three variables at a time. It returns an updated data.frame with your annotated classifications.
-#'
-#' @param data A character vector of texts you want to annotate or a data.frame returned from the handcode() function.
-#' @param ... Between one and three named character vectors indicating different variables and categories you want to use for your annotation. Only needed if data a new character vector of texts.
-#' @param start A numeric value indicating the line in which you want to start handcoding. Alternatively, you can set start to "first_empty" to automatically start handcoding in the first line that has not been handcoded yet, or to "all_empty" to automatically handcode all lines that have not been handcoded yet.
-#' @param randomize A logical value indicating whether you want to randomize the order in which texts are shown to the coder.
-#' @param context A logical value indicating whether you want the coder to see the previous and next text alongside the text that is currently coded. If TRUE, the function will show the previous and next text in light gray.
-#'
-# Importing dependencies with roxygen2
-#' @import shiny
-#' @importFrom shinyWidgets progressBar
-#' @importFrom shinyWidgets updateProgressBar
-#'
-# Export function
-#' @export
+  # Generate final data.frame to be displayed as output
+  final <- data.frame(id = data$id, texts = data$texts, kat1 = values$code1, kat2 = values$code2, kat3 = values$code3)
 
+  # Reduce to size of original dataframe
+  final <- final[,1:(ncol(data)-2)]
 
-handcode <- function(data, ... , start = "first_empty", randomize = FALSE, context = FALSE) {
+  # Take names from original dataframe
+  names(final) <- names(data)[-c(2,3)]
 
-  # Initialize ...
-  arg_list <- list(...)
+  # Make sure all NA is saved as ""
+  final[is.na(final)] <- ""
 
-  # Check if data is either data.frame or character vector of texts
-  if(!is.data.frame(data) & !is.character(data)) stop("data must be a character vector of texts you want to annotate or a data.frame() that has been returned in an earlier run of this function.")
+  # Reorder output
+  final <- final[order(final$id),]
+
+  # Delete id
+  final <- final[,-1]
+  rownames(final) <- NULL
+
+  # Return
+  return(final)
+}
 
 
-  # Checks and data handling if data is character vector
-  if(is.character(data)){
 
-    # Check if items in arg_list are named vectors
-    if(!all(vapply(arg_list, is.character, logical(1))) | !all(vapply(arg_list, is.vector, logical(1)))) {
-      stop("All arguments in ... must be named character vectors.")
-    }
+#' character_to_data: Transform text vector and arg_list to dataframe.
 
-    # Check that there are between 1 and 3 named character vectors given
-    if(length(arg_list) < 1 | length(arg_list) > 3) {
-      stop("If data is a character vector of texts to annotate, you must provide between 1 and 3 named character vectors of annotation categories.")
-    }
+character_to_data <- function(data, arg_list) {
 
-    # Check if "" and "Not applicable" are in list of categories
-    if(TRUE %in% sapply(sapply(arg_list, function(x) x %in% c("", "Not applicable")), function(x) TRUE %in% x)) stop("The default categories \"\" and \"Not applicable\" are automatically added handcode(). You cannot add them as input to the function.")
+  output <- data.frame(matrix(factor(""), nrow = length(data), ncol = length(arg_list)))
 
-    # Check for duplicate categories
-    for (i in seq_along(arg_list)) {
-      if(length(unique(arg_list[[i]]))<length(arg_list[[i]])) stop("You cannot set duplicate categories for a variable. Please provide unique categories for classification.")
-    }
+  # Name variables according to arg_list
+  names(output) <- names(arg_list)
 
-
-    # Data handling
-
-    # Initialize empty dataframe
-    output <- data.frame(matrix(factor(""), nrow = length(data), ncol = length(arg_list)))
-
-    # Name variables according to arg_list
-    names(output) <- names(arg_list)
-
-    # For every categorisation, set levels of factor
-    for (i in seq_along(arg_list)){
-      output[,i] <- factor("", levels = c("", "Not applicable", arg_list[[i]]))
-    }
-
-    # Paste texts object to dataframe
-    data <- data.frame(texts = data, output)
-
+  # For every categorisation, set levels of factor
+  for (i in seq_along(arg_list)){
+    output[,i] <- factor("", levels = c("", "Not applicable", arg_list[[i]]))
   }
 
-  # Checks -----------------------------------------------------------------------
+  # Paste texts object to dataframe
+  data <- data.frame(texts = data, output)
 
-  # Check if first column of data is texts and character
-  if(names(data)[1] != "texts" | !is.character(data[,1]) ) stop("data must be a character vector of texts you want to annotate or a data.frame() that has been returned in an earlier run of this function.")
+  return(data)
 
-  # Check if all columns except the first one are factors
-  if(!all(sapply(data[, -1], is.factor))) stop("data must be a character vector of texts you want to annotate or a data.frame() that has been returned in an earlier run of this function.")
-
-  # Check if there are min 1 and  max 3 classification variables
-  if (ncol(data) < 2 | ncol(data) > 4) stop("data must be a character vector of texts you want to annotate or a data.frame() that has been returned in an earlier run of this function.")
-
-  # check if start is a single value
-  if(length(start) > 1) stop("start must be a single value.")
-
-  # Check if start is numeric or "first_empty"
-  if(!is.numeric(start) & !start%in%c("first_empty", "all_empty")) stop("start must be numeric, 'first_empty', or 'all_empty'.")
-
-  # Check if there is uncoded data when start = "first_empty"
-  if(all(!do.call(paste0,data.frame(data[,-1], helper=""))=="")) stop("All your data is already classified. Please provide unclassified data if you want to proceed.")
-
-  # Check if randomize is single value
-  if(length(randomize)>1) stop("randomize must be a single value.")
-
-  # Check if randomize is logical
-  if(!is.logical(randomize)) stop("randomize must be either TRUE or FALSE.")
-
-  # Check if context is single value
-  if(length(context)>1) stop("context must be a single value.")
-
-  # Check if context is logical
-  if(!is.logical(context)) stop("context must be either TRUE or FALSE.")
-
-  # Check if interactive
-  if(!interactive()) stop("handcode() can only be used in an interactive R session.")
-
-  # Check if shiny is installed
-  if (system.file(package="shiny") == "") stop("handcode() requires the package shiny to be installed. You can run install.packages(\"shiny\") to install shiny.")
+}
 
 
-  # Initialize -------------------------------------------------------------------
 
-  # Initialize list a
+#' data_for_app: Prepare inputs for shiny app
+
+data_for_app <- function(data, start, randomize, context) {
+
   a <- list()
-
 
   # Add context to dataframe
   data <- data.frame(before = c("", data$texts[1:nrow(data)-1]), after = c(data$texts[2:nrow(data)], ""), data)
@@ -436,9 +353,115 @@ handcode <- function(data, ... , start = "first_empty", randomize = FALSE, conte
   a$context_app <- context
 
 
+  return(a)
+
+}
+
+
+
+#' handcode: Classifying text into pre-defined categories.
+#'
+#' `handcode` opens a Shiny app which allows for handcoding strings of text into pre-defined categories. You can code between one and three variables at a time. It returns an updated data.frame with your annotated classifications.
+#'
+#' @param data A character vector of texts you want to annotate or a data.frame returned from the handcode() function.
+#' @param ... Between one and three named character vectors indicating different variables and categories you want to use for your annotation. Only needed if data a new character vector of texts.
+#' @param start A numeric value indicating the line in which you want to start handcoding. Alternatively, you can set start to "first_empty" to automatically start handcoding in the first line that has not been handcoded yet, or to "all_empty" to automatically handcode all lines that have not been handcoded yet.
+#' @param randomize A logical value indicating whether you want to randomize the order in which texts are shown to the coder.
+#' @param context A logical value indicating whether you want the coder to see the previous and next text alongside the text that is currently coded. If TRUE, the function will show the previous and next text in light gray.
+#'
+# Importing dependencies with roxygen2
+#' @import shiny
+#' @importFrom shinyWidgets progressBar
+#' @importFrom shinyWidgets updateProgressBar
+#'
+# Export function
+#' @export
+
+
+handcode <- function(data, ... , start = "first_empty", randomize = FALSE, context = FALSE) {
+
+  # Initialize ...
+  arg_list <- list(...)
+
+
+# Checks and datahandling for data input ----------------------------------
+
+  # Check if data is either data.frame or character vector of texts
+  if(!is.data.frame(data) & !is.character(data)) stop("data must be a character vector of texts you want to annotate or a data.frame() that has been returned in an earlier run of this function.")
+
+
+  # Checks and data handling if data is character vector
+  if(is.character(data)){
+
+    # Check if items in arg_list are named vectors
+    if(!all(vapply(arg_list, is.character, logical(1))) | !all(vapply(arg_list, is.vector, logical(1)))) {
+      stop("All arguments in ... must be named character vectors.")
+    }
+
+    # Check that there are between 1 and 3 named character vectors given
+    if(length(arg_list) < 1 | length(arg_list) > 3) {
+      stop("If data is a character vector of texts to annotate, you must provide between 1 and 3 named character vectors of annotation categories.")
+    }
+
+    # Check if "" and "Not applicable" are in list of categories
+    if(TRUE %in% sapply(sapply(arg_list, function(x) x %in% c("", "Not applicable")), function(x) TRUE %in% x)) stop("The default categories \"\" and \"Not applicable\" are automatically added handcode(). You cannot add them as input to the function.")
+
+    # Check for duplicate categories
+    for (i in seq_along(arg_list)) {
+      if(length(unique(arg_list[[i]]))<length(arg_list[[i]])) stop("You cannot set duplicate categories for a variable. Please provide unique categories for classification.")
+    }
+
+    # Data handling
+    data <- character_to_data(data, arg_list)
+
+  }
+
+  # Checks -----------------------------------------------------------------------
+
+  # Check if first column of data is texts and character
+  if(names(data)[1] != "texts" | !is.character(data[,1]) ) stop("data must be a character vector of texts you want to annotate or a data.frame() that has been returned in an earlier run of this function.")
+
+  # Check if all columns except the first one are factors
+  if(!all(sapply(data[, -1], is.factor))) stop("data must be a character vector of texts you want to annotate or a data.frame() that has been returned in an earlier run of this function.")
+
+  # Check if there are min 1 and  max 3 classification variables
+  if (ncol(data) < 2 | ncol(data) > 4) stop("data must be a character vector of texts you want to annotate or a data.frame() that has been returned in an earlier run of this function.")
+
+  # check if start is a single value
+  if(length(start) > 1) stop("start must be a single value.")
+
+  # Check if start is numeric or "first_empty"
+  if(!is.numeric(start) & !start%in%c("first_empty", "all_empty")) stop("start must be numeric, 'first_empty', or 'all_empty'.")
+
+  # Check if there is uncoded data when start = "first_empty"
+  if(all(!do.call(paste0,data.frame(data[,-1], helper=""))=="")) stop("All your data is already classified. Please provide unclassified data if you want to proceed.")
+
+  # Check if randomize is single value
+  if(length(randomize)>1) stop("randomize must be a single value.")
+
+  # Check if randomize is logical
+  if(!is.logical(randomize)) stop("randomize must be either TRUE or FALSE.")
+
+  # Check if context is single value
+  if(length(context)>1) stop("context must be a single value.")
+
+  # Check if context is logical
+  if(!is.logical(context)) stop("context must be either TRUE or FALSE.")
+
+  # Check if interactive
+  if(!interactive()) stop("handcode() can only be used in an interactive R session.")
+
+
+  # Initialize -------------------------------------------------------------------
+
+  a <- data_for_app(data, start, randomize, context)
+
   # Run App ----------------------------------------------------------------------
 
   ret <- runApp(handcoder_app(a))
+
+
+# Output results ----------------------------------------------------------
 
   cat("Please cite: Isermann, Lukas. 2023. handcodeR: Text annotation app. R package version 0.1.0. https://github.com/liserman/handcodeR")
 
---
+++ tests/testthat/test_functions.R
@@ -0,0 +1,166 @@
+library(testthat)
+library(handcodeR)
+
+
+
+# gen_output --------------------------------------------------------------
+
+test_that("Test gen_output", {
+
+  data <- data.frame(id = c(2,1,3),
+                     before = c("Text 1", "", "Text 2"),
+                     after = c("Text 3", "Text 2", ""),
+                     texts = c("Text 2", "Text 1", "Text 3"),
+                     fruits = factor("", levels = c("", "Not applicable", "apple", "banana", "pear")),
+                     colors = factor("", levels = c("", "Not applicable", "green", "yellow")))
+
+  values <- list(code1 = factor(c("apple", "apple", "banana"),
+                                levels = c("", "Not applicable", "apple", "banana", "pear")),
+                 code2 = factor(c("green", "green", "yellow"),
+                                levels = c("", "Not applicable", "green", "yellow")),
+                 code3 = c("", "", ""))
+
+  output <- gen_output(data, values)
+
+  # Output is data.frame of correct dimensions
+  expect_equal(class(output), "data.frame")
+  expect_equal(dim(output), c(nrow(data), ncol(data)-3))
+
+  # Output is in order
+  expect_true(all(output$texts == data[order(data$id),"texts"]))
+
+  # Correct names
+  expect_equal(names(output), names(data)[-c(1,2,3)])
+
+})
+
+
+# character_to_data -------------------------------------------------------
+
+test_that("Test character_to_data", {
+
+  data <- c("Text 1", "Text 2", "Text 3")
+  arg_list <- list(fruits = c("apple", "banana", "pear"),
+                   colors = c("green", "yellow"))
+
+  output <- character_to_data(data, arg_list)
+
+  # Output is dataframe
+  expect_equal(class(output), "data.frame")
+
+  # Expect dimensions 3x3
+  expect_equal(dim(output), c(length(data),length(arg_list)+1))
+
+  # Row 1 is character
+  expect_equal(class(output[,1]), "character")
+
+  # Row 2 and 3 are factor
+  expect_equal(class(output[,2]), "factor")
+  expect_equal(class(output[,3]), "factor")
+
+  # Row 1 is equal data input
+  expect_equal(output[,1], data)
+
+  # Names
+  expect_equal(names(output), c("texts", names(arg_list)))
+
+  # Factor levels
+  expect_contains(levels(output[,2]), "Not applicable")
+  expect_contains(levels(output[,2]), "")
+  expect_contains(levels(output[,2]), arg_list[[1]])
+
+})
+
+
+# data_for_app ------------------------------------------------------------
+
+
+test_that("Test 1 data_for_app", {
+
+  data <- data.frame(texts = c("Text 1", "Text 2", "Text 3"),
+                     fruits = factor("", levels = c("", "Not applicable", "apple", "banana", "pear")),
+                     colors = factor("", levels = c("", "Not applicable ", "green", "yellow")))
+  start = "first_empty"
+  randomize = FALSE
+  context = FALSE
+
+  a <- data_for_app(data, start, randomize, context)
+
+  # Output is list
+  expect_equal(class(a), "list")
+
+  # Names
+  expect_equal(names(a), c("container", "data_app", "start_app", "classifications", "context_app"))
+
+  # Container
+  expect_equal(dim(a$container), c(nrow(data), 3))
+
+  # Data
+  expect_equal(names(a$data_app), c("id", "before", "after", names(data)))
+
+  # Start
+  expect_equal(a$start_app, 1)
+
+  # Classifications
+  expect_equal(names(a$classifications), names(data)[-1])
+  expect_equal(a$classifications[[1]], levels(data[,2]))
+
+  # Context
+  expect_equal(a$context_app, context)
+
+  # Ranomize
+  expect_true(all(a$data_app$id == c(1:nrow(a$data_app))))
+})
+
+
+
+test_that("Test 2 data_for_app", {
+
+  data <- data.frame(texts = c("Text 1", "Text 2", "Text 3", "Text 4"),
+                     fruits = factor("", levels = c("", "Not applicable", "apple", "banana", "pear")),
+                     colors = factor("", levels = c("", "Not applicable ", "green", "yellow")),
+                     taste = factor("", levels = c("", "Not applicable", "sweet", "sour")))
+  start = "first_empty"
+  randomize = TRUE
+  context = TRUE
+
+  a <- data_for_app(data, start, randomize, context)
+
+  # Output is list
+  expect_equal(class(a), "list")
+
+  # Names
+  expect_equal(names(a), c("container", "data_app", "start_app", "classifications", "context_app"))
+
+  # Container
+  expect_equal(dim(a$container), c(nrow(data), 3))
+
+  # Data
+  expect_equal(names(a$data_app), c("id", "before", "after", names(data)))
+
+  # Start
+  expect_equal(a$start_app, 1)
+
+  # Classifications
+  expect_equal(names(a$classifications), names(data)[-1])
+  expect_equal(a$classifications[[3]], levels(data[,4]))
+
+  # Context
+  expect_equal(a$context_app, context)
+
+  # Ranomize
+  expect_false(all(a$data_app$id == c(1:nrow(a$data_app))))
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
